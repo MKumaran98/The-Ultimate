f8cdd610d97902e2ffe262288dc23f4c105d01ca
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-04-15 00:08:56 +0530

    refactor-home page refactored

--- src/component/maintypography/maintypography.js
+++ src/component/maintypography/maintypography.js
@@ -1,15 +1,14 @@
 import classes from './maintypography.module.css'
 
-
-const MainSection=(props)=>{
+const MainSection=()=>{
     return (
-        <div className={classes.main_sec}>
-            <div className={classes.mainSectionTypography}>
+        <div className={classes["main-section"]}>
+            <div className={classes["main-section-typography"]}>
                 <h1>Hi, I'm</h1>
-                <h2 className={classes.big_heading}>Muthu Kumaran.</h2>
-                <h3 className={classes.subTitle}>And I love making stuff!</h3>
-                <p className={classes.typography}>I'm a developer based in India specializing in building exceptional webapps.</p>
-                <a href="mailto: muthu101kumaran@gmail.com" className={classes.email_link} target="_blank" rel="noreferrer">Get In Touch</a>    
+                <h2 className={classes["big-heading"]}>Muthu Kumaran.</h2>
+                <h3 className={classes["sub-title"]}>And I love making stuff!</h3>
+                <p className={classes["typography"]}>I'm a developer based in India specializing in building exceptional webapps.</p>
+                <a href="mailto: muthu101kumaran@gmail.com" className={classes["email-link"]} target="_blank" rel="noreferrer">Get In Touch</a>    
             </div>   
         </div>
     );
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import classes from './Home.module.css';
 import MainTypography from '../../component/maintypography/maintypography';
 import Wifi from '../../component/wifi/wifi';
 
-const Home=(props)=>{
+const Home=()=>{
     return(
-        <div className={classes.homeContainer}>
+        <div>
             <MainTypography/>
             <Wifi/>
         </div>
