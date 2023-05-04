0c37fde7d98953ec5ecb693f461a8fbe27849b9f
kumaranmk98 <muthu.r@unacademy.com>
2023-05-05 00:45:25 +0530

    feat: updated blogs

--- src/containers/Blogs/blogsData.js
+++ src/containers/Blogs/blogsData.js
@@ -1,14 +1,28 @@
-export const BlogData=[
-    {
-        name:"The Curious Case of The Javascript Trinity",
-        description:"A blog about how type coercion is done in javascript ",
-        image:"https://miro.medium.com/max/659/1*IFI67ilMj9D45bohehQDJg.jpeg",
-        link:"https://muthu101kumaran.medium.com/the-curious-case-of-the-javascript-trinity-5af722b52e64"
-    },
-    {
-        name:"Why Javascript is awesome and why you should learn it",
-        description:"What makes javascript so great? read an find out!",
-        image:"https://miro.medium.com/max/700/1*bxEkHw1xewxOFjmGunb-Cw.png",
-        link:"https://muthu101kumaran.medium.com/why-javascript-is-awesome-and-why-you-should-learn-it-6e52f7fac015"
-    }    
-]
+export const BlogData = [
+  {
+    name: "The Curious Case of The Javascript Trinity",
+    description: "A blog about how type coercion is done in javascript ",
+    image: "https://miro.medium.com/max/659/1*IFI67ilMj9D45bohehQDJg.jpeg",
+    link: "https://muthu101kumaran.medium.com/the-curious-case-of-the-javascript-trinity-5af722b52e64",
+  },
+  {
+    name: "Why Javascript is awesome and why you should learn it",
+    description: "What makes javascript so great? read an find out!",
+    image: "https://miro.medium.com/max/700/1*bxEkHw1xewxOFjmGunb-Cw.png",
+    link: "https://muthu101kumaran.medium.com/why-javascript-is-awesome-and-why-you-should-learn-it-6e52f7fac015",
+  },
+  {
+    name: "A brief look into Critical Rendering Path",
+    description: "How does the browser work? lets find out!",
+    image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1624726934054/ICl-U8kaH.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    link: "https://kumaranmk.hashnode.dev/a-brief-look-into-critical-rendering-path",
+  },
+  {
+    name: "Managing images in Cloudinary",
+    description: "Want to manage images easily? Cloudinary has got you covered",
+    image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1624704177660/hMa9ef26p.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    link: "https://kumaranmk.hashnode.dev/managing-images-in-cloudinary",
+  },
+];
